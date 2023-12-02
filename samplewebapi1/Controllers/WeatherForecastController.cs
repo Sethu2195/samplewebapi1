9264cd69a294cd685933be00eacf709d3f2099cd
sethu <sethusunddaresh.c@saint-gobain.com>
2023-12-02 11:53:01 +0530

    message new 1

--- samplewebapi1/Controllers/WeatherForecastController.cs
+++ samplewebapi1/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace samplewebapi1.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","new area","one add"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
